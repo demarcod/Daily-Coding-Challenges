8ccb1a2293f55a591d9a1daeb4ab35ce887ffcf4
Donnatella DeMarco <donnatella.maria@gmail.com>
2022-02-07 21:02:58 -0500

    Initial Commit

---
+++ Easy/Problem1.cpp
@@ -0,0 +1,11 @@
+/*
+Source: Daily Coding Problem
+
+*This problem was recently asked by Google.
+
+Description: Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+Example: Given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+*/
