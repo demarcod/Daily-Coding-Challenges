ad70d086c18398b48d18b68f0a25ee46ab6577d8
Donnatella DeMarco <donnatella.maria@gmail.com>
2022-02-09 22:23:00 -0500

    added a couple more test cases for problem1

--- Easy/Problem 1/Problem1.cpp
+++ Easy/Problem 1/Problem1.cpp
@@ -29,8 +29,12 @@ string doesAdd(vector<int>& nums, int k){
 
 int main()
 {
-    vector<int> thing = {10, 15, 3, 7};
-    string result = doesAdd(thing, 17);
-    cout << result << endl;
+    vector<int> nums1 = {10, 15, 3, 7};
+    vector<int> nums2 = {};
+    vector<int> nums3 = {-3, -3, 0, 7, 12};
+
+    cout << doesAdd(nums1, 17) << endl; //true
+    cout << doesAdd(nums2, 0) << endl; //false
+    cout << doesAdd(nums3, 9) << endl; //true
     return 0;
 }
