886756a390fd03d5ce19113339c8979ca3d8d58e
Donnatella DeMarco <donnatella.maria@gmail.com>
2022-02-09 20:55:30 -0500

    testing

--- Easy/Problem 1/Problem1.cpp
+++ Easy/Problem 1/Problem1.cpp
@@ -9,3 +9,19 @@ Example: Given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 Bonus: Can you do this in one pass?
 */
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+string doesAdd(vector<int>& nums, int k){
+    return nums.empty() ? "true" : "false";
+}
+
+int main()
+{
+    vector<int> thing = {};
+    string result = doesAdd(thing, 9);
+    cout << result << endl;
+    return 0;
+}
